f12139d69438724515ae4066a1dd21dbfabad79b
dixita-developer <dixirana.1991@gmail.com>
2017-06-18 17:28:33 -0400

    Main file added

--- Demo/application/Main.java
+++ Demo/application/Main.java
@@ -15,6 +15,8 @@ public class Main extends Application {
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			System.out.println("Hi this is test");
+			System.out.println("Hi this is test2");
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
